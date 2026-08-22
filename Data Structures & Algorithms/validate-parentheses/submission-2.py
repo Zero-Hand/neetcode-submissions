81cfc58b2e9df78478f7f5bfd3d0795716993acb
Zero-Hand <aryangupta107.87@gmail.com>
2026-08-22 15:20:07 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        list2 = []
+        dict1 = {'(':')',
+        '{' : '}',
+        '[' : ']'
+        }
+        
+
+        for c in s:
+            list2.append(c)
+
+            if len(list2) >= 2:
+                if list2[-2] in dict1 and dict1[list2[-2]] == list2[-1]:
+                    list2.pop()
+                    list2.pop()
+
+        return not list2
+
+
